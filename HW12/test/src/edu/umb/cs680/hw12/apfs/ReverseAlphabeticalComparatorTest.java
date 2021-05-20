2af87a87b92f467e2ab75fd7408d924acc9245a0
Abhishek Doshi <53604173+AbhishekD10@users.noreply.github.com>
2021-05-20 16:25:58 -0400

    Add files via upload

---
+++ HW12/test/src/edu/umb/cs680/hw12/apfs/ReverseAlphabeticalComparatorTest.java
@@ -0,0 +1,51 @@
+package edu.umb.cs680.hw12.apfs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+class ReverseAlphabeticalComparatorTest {
+	
+	LocalDateTime date = LocalDateTime.now();
+	 APFS getInstance = APFS.getInstance();
+	 ApfsDirectory root = (ApfsDirectory) getInstance.InitFileSystem("APFSDirectory", 2);
+	 ApfsDirectory home = new ApfsDirectory( "home", 0, date,root,"APFS",date);
+	 ApfsDirectory code = new ApfsDirectory( "code", 0, date,home,"APFS",date);
+	 ApfsDirectory applications = new ApfsDirectory( "applications", 0, date,root,"APFS",date);
+	 ApfsFile a = new ApfsFile( "a", 100, date,applications,"APFS",date);
+	 ApfsFile b = new ApfsFile( "b", 200, date,applications,"APFS",date);
+	 ApfsFile c = new ApfsFile( "c", 300, date,home,"APFS",date);
+	 ApfsFile d = new ApfsFile( "d", 400, date,home,"APFS",date);
+	 ApfsFile e = new ApfsFile( "e", 500, date,code,"APFS",date);
+	 ApfsFile f = new ApfsFile( "f", 600, date,code,"APFS",date);
+	 ApfsLink x = new ApfsLink("x",0, date, home,"APFS",date,applications);
+	 ApfsLink y = new ApfsLink("y",0,date,code,"APFS",date,f);
+
+	 @Test
+	 public void getChildrenVerification() {
+		    ApfsElement[] expected = { home,applications};
+            ApfsDirectory directory = root;
+	        List<ApfsElement> element = directory.getChildren(new ReverseAlphabeticalComparator());
+	        assertArrayEquals(expected,element.toArray() );
+	 }
+	 
+	 @Test
+	 public void getSubDirectoriesVerification() {
+		     ApfsDirectory[] expected = { home,applications};
+            ApfsDirectory directory = root;
+	        List<ApfsDirectory> element = directory.getSubDirectories(new ReverseAlphabeticalComparator());
+	        assertArrayEquals(expected,element.toArray() );
+	 }
+	 
+	 @Test
+	 public void getFilesVerification() {
+		     ApfsFile[] expected = { d,c};
+            ApfsDirectory directory = home;
+	        List<ApfsFile> element = directory.getFiles(new ReverseAlphabeticalComparator());
+	        assertArrayEquals(expected,element.toArray());
+	 }
+	
+}
---
+++ HW12/test/src/edu/umb/cs680/hw12/apfs/TimeStampComparatorTest.java
@@ -0,0 +1,48 @@
+package edu.umb.cs680.hw12.apfs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+class TimeStampComparatorTest {
+
+
+	LocalDateTime date = LocalDateTime.now();
+	 APFS getInstance = APFS.getInstance();
+	 ApfsDirectory root = (ApfsDirectory) getInstance.InitFileSystem("APFSDirectory", 2);
+	 ApfsDirectory home = new ApfsDirectory( "home", 0, date,root,"APFS",date);
+	 ApfsDirectory code = new ApfsDirectory( "code", 0, date,home,"APFS",date);
+	 ApfsDirectory applications = new ApfsDirectory( "applications", 0, date,root,"APFS",date);
+	 ApfsFile a = new ApfsFile( "a", 100, date,applications,"APFS",date);
+	 ApfsFile b = new ApfsFile( "b", 200, date,applications,"APFS",date);
+	 ApfsFile c = new ApfsFile( "c", 300, date,home,"APFS",date);
+	 ApfsFile d = new ApfsFile( "d", 400, date,home,"APFS",date);
+	 ApfsFile e = new ApfsFile( "e", 500, date,code,"APFS",date);
+	 ApfsFile f = new ApfsFile( "f", 600, date,code,"APFS",date);
+	 ApfsLink x = new ApfsLink("x",0, date, home,"APFS",date,applications);
+	 ApfsLink y = new ApfsLink("y",0,date,code,"APFS",date,f);
+
+	
+	 
+	 @Test
+	 public void getSubDirectoriesVerification() {
+		     ApfsDirectory[] expected = { code};
+            ApfsDirectory directory = home;
+	        List<ApfsDirectory> element = directory.getSubDirectories(new TimeStampComparator());
+	        assertArrayEquals(expected,element.toArray() );
+	 }
+	 
+	 @Test
+	 public void getFilesVerification() {
+		     ApfsFile[] expected = { c,d};
+            ApfsDirectory directory = home;
+	        List<ApfsFile> element = directory.getFiles(new TimeStampComparator());
+	        assertArrayEquals(expected,element.toArray());
+	 }
+	 
+
+}
