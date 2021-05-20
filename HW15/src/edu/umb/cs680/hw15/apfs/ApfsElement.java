ec6f8b81bb469c8cce5149f397e50dab42b259cc
Abhishek Doshi <53604173+AbhishekD10@users.noreply.github.com>
2021-05-20 16:22:51 -0400

    Add files via upload

---
+++ HW15/src/edu/umb/cs680/hw15/apfs/ApfsElement.java
@@ -0,0 +1,27 @@
+package edu.umb.cs680.hw15.apfs;
+
+import java.time.LocalDateTime;
+
+import edu.umb.cs680.hw15.fs.*;
+
+public abstract class ApfsElement extends FSElement {
+
+	 private String ownerName;
+	 private LocalDateTime modifiedTime;
+	 
+	 public ApfsElement( String name, int size, LocalDateTime createdTime, ApfsDirectory parent,String ownerName,LocalDateTime modifiedTime) {
+			super( name, size, createdTime,parent);
+			this.ownerName = ownerName;
+			this.modifiedTime = modifiedTime;
+		}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public LocalDateTime getModifiedTime() {
+		return modifiedTime;
+	}
+	  
+
+}
